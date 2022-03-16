09bd3a77d8ae91ac124fb6f6e44c9a45584c375d
likhithar2507 <likhithagowda2507@gmail.com>
2022-03-16 23:03:54 +0530

    Multiplication

---
+++ src/com/harman/Gitversioning/Multiplication.java
@@ -0,0 +1,10 @@
+package com.harman.Gitversioning;
+
+public class Multiplication {
+    public static void main(String[] args){
+        int x=7;
+        int y=2;
+        int z= x*y;
+        System.out.println(z);
+    }
+}

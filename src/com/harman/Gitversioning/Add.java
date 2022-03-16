51c608849274c066fd9747c9bb02563aeb294ad3
likhithar2507 <likhithagowda2507@gmail.com>
2022-03-16 22:51:16 +0530

    Addition

---
+++ src/com/harman/Gitversioning/Add.java
@@ -0,0 +1,10 @@
+package com.harman.Gitversioning;
+
+public class Add {
+    public static void main(String[] args){
+    int x=7;
+    int y=6;
+    int z=x+y;
+    System.out.println(z);
+}
+    }
